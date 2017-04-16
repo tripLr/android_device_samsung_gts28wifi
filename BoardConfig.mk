30185e5f1f7ce5964943231765f41ad529cf7916
Jan Huijsmans <huysmans@koffie.nu>
2017-04-16 11:43:37 +0200

    gts28wifi: Immediately evaluate/set BOARD_SEPOLICY_DIRS
    Otherwise we will lose all our sepolicies because the
    BOARD_SEPOLICY_DIRS variable is not populated at this
    point and thus cannot be extended.
    
    Change-Id: I692dd00607910080019fb01ecbcc22c74fe2956b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,8 @@ TARGET_KERNEL_CONFIG := cm_exynos5433-gts28wifi_defconfig
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := gts28wifi,gts28wifixx
 
+# SELinux
+BOARD_SEPOLICY_DIRS := $(LOCAL_PATH)/sepolicy
+
 # Inherit common board flags
 include device/samsung/gts2-common/BoardConfigCommon.mk
