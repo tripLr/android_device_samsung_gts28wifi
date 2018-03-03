9d3956c5aebb87a929f33403e2a193eacbc9d531
Christopher N. Hesse <raymanfx@gmail.com>
2018-03-04 00:02:31 +0100

    gts28wifi: Shuffle common tree inclusion
    Change-Id: I34e2ddb6cd5826a055f49ab59cfbf33d3fc0c083

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,10 @@
+# Inherit common board flags
+include device/samsung/gts2-common/BoardConfigCommon.mk
+
 LOCAL_PATH := device/samsung/gts28wifi
 
 # Include path
-TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 
 # Kernel
 TARGET_KERNEL_CONFIG := cm_exynos5433-gts28wifi_defconfig
@@ -15,7 +18,4 @@ RECOVERY_TOUCHSCREEN_FLIP_Y := true
 BOARD_SEPOLICY_DIRS := $(LOCAL_PATH)/sepolicy
 
 # SELinux
-BOARD_SEPOLICY_DIRS := $(LOCAL_PATH)/sepolicy
-
-# Inherit common board flags
-include device/samsung/gts2-common/BoardConfigCommon.mk
+BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
